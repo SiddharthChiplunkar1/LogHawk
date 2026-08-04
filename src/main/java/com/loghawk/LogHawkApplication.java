a83334d627a0723bdebb5df335b2fe539b73ebc0
Siddharth <admin@campusearn.com>
2026-08-04 20:49:18 +0530

    Enable Async in Main Application

--- src/main/java/com/loghawk/LogHawkApplication.java
+++ src/main/java/com/loghawk/LogHawkApplication.java
@@ -2,8 +2,10 @@ package com.loghawk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class LogHawkApplication {
 
 	public static void main(String[] args) {
