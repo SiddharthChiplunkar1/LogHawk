dfbd0dc39d0f2f8eb3c93aa7bb4d1de5e6f23319
Siddharth <admin@campusearn.com>
2026-08-04 20:27:15 +0530

    Basic Application Structure

---
+++ src/main/java/com/loghawk/LogHawkApplication.java
@@ -0,0 +1,13 @@
+package com.loghawk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogHawkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogHawkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/loghawk/LogHawkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loghawk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogHawkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
